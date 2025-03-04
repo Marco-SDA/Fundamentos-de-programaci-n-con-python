c3d1eae53cf08adbf3cc64f4216b7457e7a2bb41
Marquitos <jean2250161@correo.uis.edu.co>
2025-03-04 10:41:31 -0500

    Agregué Números devueltos en orden contrario

---
+++ Números al revez.py
@@ -0,0 +1,11 @@
+n = int(input("ingrese n"))
+nbak = n
+m= 0
+while n > 0:
+    r = n % 10
+    n = n // 10
+    m = m * 10 + r
+
+print (str(m) + " -> " + str(nbak))
+print(f"{nbak} -> {m}")
+print("{} -> {}".format(m, nbak))   
